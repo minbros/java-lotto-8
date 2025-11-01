1a0df4a8e6fd14b6c89348a6879e3a2b9a3288e1
minbros <pristo24631@gmail.com>
2025-11-01 17:07:03 +0900

    refactor: 검증 메서드를 package private으로 수정

--- src/main/java/lotto/domain/LottoRules.java
+++ src/main/java/lotto/domain/LottoRules.java
@@ -8,19 +8,19 @@ public final class LottoRules {
     public static final int MINIMUM_NUMBER = 1;
     public static final int MAXIMUM_NUMBER = 45;
 
-    public static boolean hasInvalidCount(List<Integer> numbers) {
+    static boolean hasInvalidCount(List<Integer> numbers) {
         return numbers.size() != NUMBER_COUNT;
     }
 
-    public static boolean hasDuplicate(List<Integer> numbers) {
+    static boolean hasDuplicate(List<Integer> numbers) {
         return numbers.size() != new HashSet<>(numbers).size();
     }
 
-    public static boolean hasInvalidValue(List<Integer> numbers) {
+    static boolean hasInvalidValue(List<Integer> numbers) {
         return numbers.stream().anyMatch(LottoRules::isOutOfRange);
     }
 
-    public static boolean isOutOfRange(int number) {
+    static boolean isOutOfRange(int number) {
         return number < MINIMUM_NUMBER || number > MAXIMUM_NUMBER;
     }
 
