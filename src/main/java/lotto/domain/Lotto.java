20475d241912759917cb72b69e698741f9340c50
minbros <pristo24631@gmail.com>
2025-11-01 14:31:35 +0900

    feat: 로또에 중복 검증 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import lotto.exception.ErrorMessage;
 import lotto.exception.LottoException;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -16,6 +17,9 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new LottoException(ErrorMessage.LOTTO_INVALID_NUMBER_COUNT.getMessage());
+        }
+        if (numbers.size() != new HashSet<>(numbers).size()) {
+            throw new LottoException(ErrorMessage.LOTTO_DUPLICATE_NUMBERS.getMessage());
         }
     }
 }
--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -1,6 +1,7 @@
 package lotto.exception;
 
 public enum ErrorMessage {
+    LOTTO_DUPLICATE_NUMBERS("중복되는 로또 번호가 있습니다."),
     LOTTO_INVALID_NUMBER_COUNT("로또 번호는 6개여야 합니다.");
 
     private static final String ERROR_PREFIX = "[ERROR] ";
