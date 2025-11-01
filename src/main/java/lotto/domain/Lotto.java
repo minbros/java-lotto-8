fedeb6dc2b90bf8a86d21eb0157b838201abd65a
minbros <pristo24631@gmail.com>
2025-11-01 14:47:04 +0900

    refactor: Lotto 상수 리팩터링

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -7,8 +7,9 @@ import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
-    private static final int LOTTO_MINIMUM = 1;
-    private static final int LOTTO_MAXIMUM = 45;
+    private static final int LOTTO_SIZE = 6;
+    private static final int LOTTO_MINIMUM_VALUE = 1;
+    private static final int LOTTO_MAXIMUM_VALUE = 45;
 
     private final List<Integer> numbers;
 
@@ -18,13 +19,13 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_SIZE) {
             throw new LottoException(ErrorMessage.LOTTO_INVALID_NUMBER_COUNT.getMessage());
         }
         if (numbers.size() != new HashSet<>(numbers).size()) {
             throw new LottoException(ErrorMessage.LOTTO_DUPLICATE_NUMBERS.getMessage());
         }
-        if (numbers.stream().anyMatch(num -> num < LOTTO_MINIMUM || num > LOTTO_MAXIMUM)) {
+        if (numbers.stream().anyMatch(num -> num < LOTTO_MINIMUM_VALUE || num > LOTTO_MAXIMUM_VALUE)) {
             throw new LottoException(ErrorMessage.LOTTO_INVALID_NUMBER_VALUE.getMessage());
         }
     }
