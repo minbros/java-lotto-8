9430637555d6efec10dc17d3d81c371623753419
minbros <pristo24631@gmail.com>
2025-11-02 00:41:19 +0900

    refactor: 구매 금액 입력 테스트 수정

--- src/test/java/lotto/controller/stage/AmountInputStageTest.java
+++ src/test/java/lotto/controller/stage/AmountInputStageTest.java
@@ -1,13 +1,9 @@
 package lotto.controller.stage;
 
-import lotto.exception.ErrorMessage;
 import lotto.view.FakeInputView;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class AmountInputStageTest {
     @Test
@@ -20,18 +16,14 @@ class AmountInputStageTest {
         assertThat(amount).isEqualTo(8000);
     }
 
-    @ParameterizedTest
-    @CsvSource({
-            "7500, AMOUNT_DOES_NOT_FIT",
-            "10000000, AMOUNT_TOO_LARGE"    // 천만 원
-    })
-    void 잘못된_구매_금액을_입력받으면_예외가_발생한다(String input, String errorKey) {
-        FakeInputView inputView = new FakeInputView(input);
+    @Test
+    void 잘못된_구매_금액을_입력받으면_예외가_발생한다() {
+        String[] inputs = new String[]{"10000000", "9500", "8000"};
+        FakeInputView inputView = new FakeInputView(inputs);
         AmountInputStage stage = new AmountInputStage(inputView);
 
-        String expectedMessage = ErrorMessage.valueOf(errorKey).getMessage();
-        assertThatThrownBy(() -> stage.execute(null))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(expectedMessage);
+        int amount = stage.execute(null);
+
+        assertThat(amount).isEqualTo(8000);
     }
 }
