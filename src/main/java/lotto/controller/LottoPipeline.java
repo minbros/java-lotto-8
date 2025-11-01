428516999e1f6dd3bfe286e18f282268c2648028
minbros <pristo24631@gmail.com>
2025-11-01 19:04:05 +0900

    feat: LottoPipeline 구현

---
+++ src/main/java/lotto/controller/LottoPipeline.java
@@ -0,0 +1,28 @@
+package lotto.controller;
+
+import lotto.controller.stage.PipeLineStage;
+
+import java.util.function.Function;
+
+public class LottoPipeline<I, O> {
+    private final Function<I, O> function;
+
+    public LottoPipeline(Function<I, O> function) {
+        this.function = function;
+    }
+
+    public static <I, O> LottoPipeline<I, O> of(PipeLineStage<I, O> stage) {
+        return new LottoPipeline<>(stage::execute);
+    }
+
+    public <N> LottoPipeline<I, N> then(PipeLineStage<O, N> nextStage) {
+        return new LottoPipeline<>(input -> {
+            O middle = execute(input);
+            return nextStage.execute(middle);
+        });
+    }
+
+    public O execute(I input) {
+        return function.apply(input);
+    }
+}
