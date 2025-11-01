784728c7b4448f4841c1322ffdecfd6219d15c5b
minbros <pristo24631@gmail.com>
2025-11-02 01:59:21 +0900

    feat: 로또 번호 생성 구현

--- src/main/java/lotto/controller/stage/LottoGenerationStage.java
+++ src/main/java/lotto/controller/stage/LottoGenerationStage.java
@@ -1,8 +1,10 @@
 package lotto.controller.stage;
 
 import lotto.domain.Lotto;
+import lotto.domain.LottoGenerator;
 import lotto.view.OutputView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @SuppressWarnings("ClassCanBeRecord")
@@ -14,7 +16,13 @@ public class LottoGenerationStage implements Stage<Integer, List<Lotto>> {
     }
 
     @Override
-    public List<Lotto> execute(Integer input) {
-        return List.of();
+    public List<Lotto> execute(Integer count) {
+        List<Lotto> lottoList = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            Lotto lotto = LottoGenerator.generate();
+            lottoList.add(lotto);
+        }
+        outputView.printLottoList(lottoList);
+        return lottoList;
     }
 }
