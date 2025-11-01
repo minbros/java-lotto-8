c18ee1f3420ca0c458261b7f5cd8d52e93c1ba2c
minbros <pristo24631@gmail.com>
2025-11-01 17:03:29 +0900

    test: 당첨 결과를 구하는 테스트 추가

--- src/main/java/lotto/domain/WinningNumber.java
+++ src/main/java/lotto/domain/WinningNumber.java
@@ -5,7 +5,7 @@ import lotto.exception.LottoException;
 
 import java.util.List;
 
-import static lotto.domain.LottoRules.*;
+import static lotto.domain.LottoRules.isOutOfRange;
 
 public record WinningNumber(Lotto lotto, int bonusNumber) {
     public WinningNumber(List<Integer> numbers, int bonusNumber) {
@@ -14,6 +14,14 @@ public record WinningNumber(Lotto lotto, int bonusNumber) {
 
     public WinningNumber {
         validateBonusNumber(lotto, bonusNumber);
+    }
+
+    public int getMatchCount(Lotto otherLotto) {
+        return 0;
+    }
+
+    public boolean matchesBonus(Lotto otherLotto) {
+        return false;
     }
 
     private void validateBonusNumber(Lotto lotto, int bonusNumber) {
--- src/test/java/lotto/domain/WinningNumberTest.java
+++ src/test/java/lotto/domain/WinningNumberTest.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import lotto.exception.ErrorMessage;
 import lotto.exception.LottoException;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -9,15 +10,15 @@ import java.util.List;
 
 import static lotto.domain.LottoRules.MAXIMUM_NUMBER;
 import static lotto.domain.LottoRules.MINIMUM_NUMBER;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class WinningNumberTest {
-    private final List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+    private final Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
 
     @ParameterizedTest
     @ValueSource(ints = {MINIMUM_NUMBER - 1, MAXIMUM_NUMBER + 1})
     void 보너스_번호가_적합하지_않으면_예외가_발생한다(int bonusNumber) {
-        assertThatThrownBy(() -> new WinningNumber(numbers, bonusNumber))
+        assertThatThrownBy(() -> new WinningNumber(lotto, bonusNumber))
                 .isInstanceOf(LottoException.class)
                 .hasMessageContaining(ErrorMessage.BONUS_INVALID_NUMBER_VALUE.getMessage());
     }
@@ -25,8 +26,19 @@ class WinningNumberTest {
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3, 4, 5, 6})
     void 보너스_번호가_당첨_번호와_겹치면_예외가_발생한다(int bonusNumber) {
-        assertThatThrownBy(() -> new WinningNumber(numbers, bonusNumber))
+        assertThatThrownBy(() -> new WinningNumber(lotto, bonusNumber))
                 .isInstanceOf(LottoException.class)
                 .hasMessageContaining(ErrorMessage.BONUS_DUPLICATE_NUMBERS.getMessage());
+    }
+
+    @Test
+    void 당첨_결과가_정확한지_확인한다() {
+        WinningNumber winningNumber = new WinningNumber(List.of(3, 4, 5, 6, 7, 8), 1);
+
+        int matchCount = winningNumber.getMatchCount(lotto);
+        boolean matchesBonus = winningNumber.matchesBonus(lotto);
+
+        assertThat(matchCount).isEqualTo(4);
+        assertThat(matchesBonus).isTrue();
     }
 }
